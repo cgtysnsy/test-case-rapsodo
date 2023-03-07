04e6ebe695c20ce51645a5272424891af0b37ca7
Cagatay <cgtyeo@gmail.com>
2023-03-07 21:19:25 +0300

    feat(ASM-1): Set up a Vuex store to manage application state

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state() {
+    return {
+      // Your state properties here
+    };
+  },
+  mutations: {
+    // Your mutations here
+  },
+  actions: {
+    // Your actions here
+  },
+});
