bd3a6698fa747027776b580df4309dad5ab71185
Cagatay <cgtyeo@gmail.com>
2023-03-07 20:17:18 +0300

    feat(UI-2): Create home page with a list of available items

--- src/components/ProductPage.vue
+++ src/components/ProductPage.vue
@@ -1,8 +1,15 @@
 <template>
-  <v-col cols="12" sm="6" md="4" lg="3">
-    <v-card class="mx-auto" max-width="240">
+  <v-col
+    v-for="product in products"
+    :key="product.name"
+    cols="12"
+    sm="6"
+    md="4"
+    lg="3"
+  >
+    <v-card class="mx-auto" max-width="240" :product="product">
       <v-img
-        class="align-end text-black card-image"
+        class="align-end text-black"
         width="240"
         height="240"
         src="https://wiki.kerbalspaceprogram.com/images/c/ce/Image_needed.svg"
@@ -11,23 +18,21 @@
       >
       </v-img>
 
-      <v-card-title>product name</v-card-title>
+      <v-card-title>{{ product.name }}</v-card-title>
 
       <v-card-text>
-        <p>Size</p>
-        <p class="my-2">Color</p>
-        <p>$price</p>
+        <p>Size: {{ product.size }}</p>
+        <p class="my-2">Color: {{ product.color }}</p>
+        <p>${{ product.amount }}</p>
       </v-card-text>
 
-      <v-card-actions>
-        <v-btn class="btn-add">Add to Cart</v-btn>
+      <v-card-actions v-if="!product.showInput">
+        <v-btn>Add to Cart</v-btn>
       </v-card-actions>
-      <v-card-actions>
+      <v-card-actions v-else>
         <v-row>
           <v-col cols="12" class="d-flex justify-center align-center">
-            <button class="btn-product">-</button>
-
-            <input type="number" class="d-block input-product" />
+            <button>-</button>
 
             <button class="btn-product">+</button>
           </v-col>
@@ -37,7 +42,59 @@
   </v-col>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        {
+          amount: 125,
+          color: "Black",
+          name: "Product-1",
+          size: "XL",
+          stock: 2,
+        },
+        {
+          amount: 55,
+          color: "Green",
+          name: "Product-2",
+          size: "L",
+          stock: 4,
+        },
+        {
+          amount: 25,
+          color: "Red",
+          name: "Product-3",
+          size: "XXL",
+          stock: 0,
+        },
+        {
+          amount: 65,
+          color: "Yellow",
+          name: "Product-4",
+          size: "S",
+          stock: 8,
+        },
+        {
+          amount: 15,
+          color: "Gray",
+          name: "Product-5",
+          size: "M",
+          stock: 1,
+        },
+        {
+          amount: 5,
+          color: "Blue",
+          name: "Product-6",
+          size: "L",
+          stock: 0,
+        },
+      ],
+    };
+  },
+};
+//this data will be used for just for mock.
+</script>
 
 <style scoped>
 .btn-add {
