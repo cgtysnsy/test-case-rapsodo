b2ad5dd9c638c99db6daf153caca285b3c1a7959
Cagatay <cgtyeo@gmail.com>
2023-03-09 15:35:21 +0300

    feat(BI-3): Loading screen add

--- src/components/ProductPage.vue
+++ src/components/ProductPage.vue
@@ -1,56 +1,69 @@
 <template>
-  <v-col
-    v-for="product in products"
-    :key="product.id"
-    cols="12"
-    sm="6"
-    md="4"
-    lg="3"
+  <v-container
+    v-if="isLoading"
+    fluid
+    class="ma-auto d-flex flex-column justif-center align-center text-center h-100"
   >
-    <v-card class="mx-auto" max-width="240" :product="product">
-      <v-img
-        class="align-end text-black product-image"
-        width="240"
-        height="240"
-        src="https://wiki.kerbalspaceprogram.com/images/c/ce/Image_needed.svg"
-        alt="240x240"
-        cover
+    <h1>Loading...</h1>
+  </v-container>
+  <v-container v-else fluid>
+    <v-row>
+      <v-col
+        v-for="product in products"
+        :key="product.id"
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3"
       >
-      </v-img>
+        <v-card class="mx-auto" max-width="240" :product="product">
+          <v-img
+            class="align-end text-black product-image"
+            width="240"
+            height="240"
+            src="https://wiki.kerbalspaceprogram.com/images/c/ce/Image_needed.svg"
+            alt="240x240"
+            cover
+          >
+          </v-img>
 
-      <v-card-title>{{ product.name }}</v-card-title>
+          <v-card-title>{{ product.name }}</v-card-title>
 
-      <v-card-text>
-        <p>Size: {{ product.size }}</p>
-        <p class="my-2">Color: {{ product.color }}</p>
-        <p>${{ product.amount }}</p>
-      </v-card-text>
+          <v-card-text>
+            <p>Size: {{ product.size }}</p>
+            <p class="my-2">Color: {{ product.color }}</p>
+            <p>${{ product.amount }}</p>
+          </v-card-text>
 
-      <v-card-actions v-if="!product.showInput">
-        <v-btn @click="addToCart(product)" class="btn-add">Add to Cart</v-btn>
-      </v-card-actions>
-      <v-card-actions v-else>
-        <v-row>
-          <v-col cols="12" class="d-flex justify-center align-center">
-            <button @click="decreaseQuantity(product)" class="btn-product">
-              -
-            </button>
+          <v-card-actions v-if="!product.showInput">
+            <v-btn @click="addToCart(product)" class="btn-add"
+              >Add to Cart</v-btn
+            >
+          </v-card-actions>
+          <v-card-actions v-else>
+            <v-row>
+              <v-col cols="12" class="d-flex justify-center align-center">
+                <button @click="decreaseQuantity(product)" class="btn-product">
+                  -
+                </button>
 
-            <input
-              type="number"
-              v-model="product.quantity"
-              @change="updateQuantity(product, $event.target.value)"
-              class="d-block input-product"
-            />
+                <input
+                  type="number"
+                  v-model="product.quantity"
+                  @change="updateQuantity(product, $event.target.value)"
+                  class="d-block input-product"
+                />
 
-            <button @click="increaseQuantity(product)" class="btn-product">
-              +
-            </button>
-          </v-col>
-        </v-row></v-card-actions
-      >
-    </v-card>
-  </v-col>
+                <button @click="increaseQuantity(product)" class="btn-product">
+                  +
+                </button>
+              </v-col>
+            </v-row></v-card-actions
+          >
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -58,7 +71,7 @@ import * as vuex from "vuex";
 
 export default {
   computed: {
-    ...vuex.mapState(["products", "cartItems"]),
+    ...vuex.mapState(["products", "cartItems", "isLoading"]),
   },
   methods: {
     ...vuex.mapActions([
