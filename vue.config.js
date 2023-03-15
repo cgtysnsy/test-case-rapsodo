7ce1b195b9e8716c4dcaba9d37c841a6ae8ee5d3
Cagatay <cgtyeo@gmail.com>
2023-03-15 13:28:13 +0300

    modified: vue/vite config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/test-case-rapsodo/" : "/",
+  // publicPath:
+  //   process.env.NODE_ENV === "production" ? "/test-case-rapsodo/" : "/",
   transpileDependencies: true,
 });
