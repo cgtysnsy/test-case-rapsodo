89a2883c8ec086fedc746bb4f15a6329b26c2abf
Cagatay <cgtyeo@gmail.com>
2023-03-17 23:23:31 +0300

    feat: tooltip changed to v-snackbar

--- src/components/TipModal.vue
+++ src/components/TipModal.vue
@@ -1,20 +1,28 @@
 <template>
-  <v-dialog v-model="isOpen" class="model-dialog">
-    <v-card>
-      <v-card-title class="model-message">
-        <p class="title-p">{{ message }}</p>
-      </v-card-title>
-      <v-card-actions>
-        <!-- <v-btn color="primary" text @click="closeModal"> OK </v-btn> -->
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+  <div class="text-center">
+    <v-snackbar
+      v-model="isOpen"
+      rounded="pill"
+      top
+      outlined
+      :timeout="timeOutDuration"
+    >
+      {{ message }}
+
+      <template v-slot:actions>
+        <v-btn color="blue" variant="text" @click="closeModal()"> Close </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
-// import * as vuex from "vuex";
-
 export default {
+  data() {
+    return {
+      timeOutDuration: 4000,
+    };
+  },
   computed: {
     // ...vuex.mapState(["isOpen", "message"]),
     isOpen() {
@@ -33,24 +41,28 @@ export default {
 };
 </script>
 <style scope>
-.model-dialog {
+/* .model-dialog {
   /* width: 100%;
   text-align: center;
   height: 120px;
   margin: 0 auto;
   overflow-y: hidden;
-   */
-  position: fixed;
-  bottom: 0;
-  right: 0;
-}
-.model-message {
+
+} */
+/* .model-message {
   text-align: center;
   height: auto;
   width: 100%;
   height: 190px;
-}
+/* } */
 .title-p {
   font-size: 18px;
+}
+*/ .custom-snackbar {
+  top: 80px !important;
+  right: 16px !important;
+  bottom: auto !important;
+  left: auto !important;
+  transform: none !important;
 }
 </style>
