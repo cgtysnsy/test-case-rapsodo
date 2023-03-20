c384b53723e50b41aa9bfc0ed773e39994663631
Cagatay <cgtyeo@gmail.com>
2023-03-20 14:52:40 +0300

    bugfix: tipmodel.vue "/" error fixed for building

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,10 +8,9 @@ import { VTooltip } from "vuetify/lib/components/VTooltip";
 import { VContainer, VIcon } from "vuetify/lib/components";
 import { fa } from "vuetify/iconsets/fa";
 import { aliases, mdi } from "vuetify/lib/iconsets/mdi";
-import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
+import "@mdi/font/css/materialdesignicons.css";
 
 export default createVuetify({
-  // Your Vuetify options here
   icons: {
     defaultSet: "mdi",
     aliases,
